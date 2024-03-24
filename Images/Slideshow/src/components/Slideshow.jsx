cf79f20b82bb5ed5dc2275e0f7168658c13f67e3
Sriramprasath <sriramprasath04@gmail.com>
2024-03-24 22:17:02 +0530

    Slideshow

--- Images/Slideshow/src/components/Slideshow.jsx
+++ Images/Slideshow/src/components/Slideshow.jsx
@@ -5,26 +5,28 @@ import Arrow from '/arrow1.png';
 const Slideshow = ()=>{
     const [indx, setIndx] = useState(0);
 
-    const back = ()=>{
-        setIndx(indx === 0? images.length-1 : indx-1)
-    }
-    
-    const frwd = () =>{
-        setIndx(indx === images.length1-1? 0 : indx+1);
-    }
-
     return (
         <div className="slide-show-container">
-            <div className='arrow left'>
-                <img src={Arrow} onClick={back()}/>
+            <div className='arrow left' onClick={
+                ()=>{
+                    setIndx(indx === 0? images.length-1 : indx-1)
+                }
+            }>
+                <img src={Arrow}/>
             </div>
 
-            {
-                <img src={images[indx]} key={indx} className='image'/>
-            }
+            <div className='image-container'>
+                {
+                    <img src={images[indx]} key={indx} className='image'/>
+                }
+            </div>
             
-            <div className='arrow right'>
-                <img src={Arrow} onClick={frwd()}/>
+            <div className='arrow right' onClick={
+                () =>{
+                    setIndx(indx === images.length-1? 0 : indx+1);
+                }
+            }>
+                <img src={Arrow}/>
             </div>
         </div>
     );
